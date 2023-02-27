0e251c73f71e89ef26b8440fe6b4ece9c0e8fd74
Engel62 <sr.engel@bk.ru>
2023-02-27 15:26:55 +0300

    1 commit

---
+++ src/main/java/pro/sky/budgetapp/controllers/FirstController.java
@@ -0,0 +1,17 @@
+package pro.sky.budgetapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+    @GetMapping("/")
+    public String helloWord() {
+        return "Hello, web!";
+    }
+    @GetMapping( "/path/to/page")
+    public String page(@RequestParam String page) {
+        return "page:" + page;
+    }
+}
