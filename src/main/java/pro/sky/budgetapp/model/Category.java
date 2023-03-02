6b7ab892d63c6cd78a09c71859c613026dcbb5d7
Engel62 <sr.engel@bk.ru>
2023-03-02 13:52:22 +0300

    2

---
+++ src/main/java/pro/sky/budgetapp/model/Category.java
@@ -0,0 +1,6 @@
+package pro.sky.budgetapp.model;
+
+public enum Category {
+
+    FOOD,CLOTHES,FUN,TRANSPORT,HOBBY
+}
---
+++ src/main/java/pro/sky/budgetapp/model/Transaction.java
@@ -0,0 +1,13 @@
+package pro.sky.budgetapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Transaction {
+    private Category category;
+    private int sum;
+    private String comment;
+
+}
